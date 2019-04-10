cf9f357828fad63a5ab4e0258ad8439790cb6a71
Kirill <kirillyat@gmail.com>
2019-04-11 00:06:57 +0300

    Update sort.asm

--- sort.asm
+++ sort.asm
@@ -7,13 +7,41 @@ include console.inc
 .code
 
     PROC fun:
-        push EBP
-        mov EBP, ESP
+
+        ;СОГЛАШЕИЕ О СВЯЗЯХ
+        ; EBX - Начало Массива
+        ; N - размер массича
 
 
+        ;АЛОГОРИТМ ПУЗЫРЬКА НА НОРМАЛЬНОМ ЯЗЫКЕ(ПИТОН)
+        ;for i in range(len(massive)-1):
+        ;   for j in range(len(massive)-1-i):
+        ;       if massive[j] > massive[j+1]:
+        ;           massive[j], massive[j+1] = massive[j+1], massive[j]
+
+
+        mov ECX, N
+
+        sotri:
+            push ECX
+            mov ECX, N
+            sortj:
+                cmp[EBX-4*ECX], [EBX-4*(ECX-1)]
+                JG swap
+                next:
+                loop sortj
+            pop ECX
+            loop sorti
 
         RET
-        fun ENDP
+
+        swap:
+            xchg ECX, [EBX-4*ECX]
+            xcng ECX, [EBX-4*(ECX-1)]
+            xchg ECX, [EBX-4*ECX]
+            JMP next
+
+    fun ENDP
 
 
 
@@ -25,7 +53,7 @@ START:
 
 
         mov EBX, ESP
-        add EBX, 4 ;ТК первый элемент будет располагаться по след. адресу
+        sub EBX, 4 ; ТК первый элемент будет располагаться по след. адресу
 
 
     ;ВВОДИМ МАССИВ
