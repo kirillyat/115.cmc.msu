0aaa3f113106a97820c6ac50a6392048df0d665a
Kirill <kirillyat@gmail.com>
2019-04-18 21:24:56 +0300

    Update litera.asm

--- litera.asm
+++ litera.asm
@@ -2,8 +2,9 @@ include console.inc
 
 .data
 
-    text dB 101 DUP(?)
-    a db 97
+    text dB 201 DUP(?) ; 200 for 2rule 1 for size
+    a dB 97
+    alphabet dB 28 DUP(0) ; for checkFunc
     
 .code
 
@@ -72,33 +73,117 @@ include console.inc
         push EDX
         
         mov EBX, 0d
-        mov EDX, 1db
+        mov EDX, 1d
         mov ECX, 0
-        mov cl, text[0]
+        mov CL, text[0]
         
+        parsing:
+            mov BL, text[EDX]
+            cmp BL, 65
+            JB conty
+            cmp BL, 90
+            JA conty
+            ADD text[EDX], 32
+            
+            conty:
+            inc EDX
+        loop parsing
         
         pop EDX
         pop ECX
         pop EBX
-    RET
+        RET
     RuleOne endp
-    
-    
-    
-    
     
     RuleTwo proc
         outstrln "Rule #2"
-    
-    RET
+        
+        push EBX
+        push ECX
+        push EDX
+        
+        mov EBX, 0d
+        mov EDX, 1d
+        mov ECX, 0
+        mov CL, text[0]
+        
+        parsing:
+            mov BL, text[EDX]
+            cmp BL, 65
+            JB conty
+            cmp BL, 90
+            JA conty
+            ADD text[EDX], 32
+            
+            conty:
+            inc EDX
+        loop parsing
+        
+        pop EDX
+        pop ECX
+        pop EBX
+        RET
+
     RuleTwo endp
+    
+    
+    
+    checkFunc proc
+    
+        
+        push EBX
+        push ECX
+        push EDX
+        
+        mov EBX, 0d
+        mov EDX, 1d
+        mov ECX, 0
+        mov CL, text[0]
+        
+        parsing:
+            mov BL, text[EDX]
+            cmp BL, 65
+            JB NO
+            cmp BL, 90
+            JA MeyBE
+            inc alphabet[EBX - "A" + 1 ]
+            jmp NO
+          MeyBE:
+            cmp BL, 97
+            JB NO
+            cmp BL, 123
+            JA NO
+            dec alphabet[EBX - "A" - 31]
+          NO:
+            inc EDX
+        loop parsing
+        
+        mov ECX, 26
+        
+        A:
+            cmp alphabet[ECX], 0
+            JZ R
+            call RuleTwo
+            jmp final
+            R:
+        loop A
+        
+        CALL RuleOne
+        final:
+        pop EDX
+        pop ECX
+        pop EBX
+        RET
+    checkFunc endp
+    
+    
 
 
 START:
     
-    outchar a
-    sub a, 32
-    outchar a
-
+    call read
+    call checkFunc
+    call write
+    
 EXIT
 end START
