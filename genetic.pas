255fbdf5f949bc158465ef49d7ba693987e1ebe2
Kirill <kirillyat@gmail.com>
2019-05-14 08:14:15 +0300

    Update genetic.pas

--- genetic.pas
+++ genetic.pas
@@ -10,211 +10,188 @@ program genetic;
 
 
 const
-    n = 20;         {Число особей}
-    OSTANOV = 4000;  {Проходов до остановы}
-    EXACT = 16384;  {Точность}
-    v1 = 0.30;
+      n=10; {Число особей}
+      s=16;
+      esp=1/16384; {Точность}
+      maxim=1.6054; {Максимальное значение}
 
 type
-    ArrF = array[1..n] of real;            {Значения функции}
-    individ = array[1..n] of integer;      {Двоичная запись одного гена}
-    population = array[1..n] of individ;   {Массив все генов}
+    mb=array [1..n,1..s] of byte; {Двоичная запись чисел}
+    mi=array[1..n] of real; {Обычный массив чисел}
 
 var
-    pop: population;
-    mx: real;
+    x, nx, y:mi;
+    x2,nx2:mb;
+    i, k, j:integer;
+    e, z:real;
 
 
-function F(x: real): real;
+function f(x: real): real;
 begin
-    F := (x-2)*(x-2.5)*(x-3)*(x-3.5)*(1-exp(x-1.5))*ln(x+0.5);
+    f:= (x-2)*(x-2.5)*(x-3)*(x-3.5)*(1-exp(x-1.5))*ln(x+0.5);
 end;
 
 
-procedure dec2bin(x: real; var a: individ);
-var i: integer;
+{Ввод изначального массива}
+procedure INIT(var a:mi);
+    var i : integer;
 begin
-    case trunc(x) of
-        1: begin a[1] := 0; a[2] := 1; end;
-        2: begin a[1] := 1; a[2] := 0; end;
-        3: begin a[1] := 1; a[2] := 1; end;
-    end;
-    x := x - trunc(x);
-    for i := 3 to n do begin
-        a[i] := trunc(2 * x);
-        x := 2 * x - trunc(2 * x);
-    end;
+    for i:=1 to n do
+	  a[i]:=random(maxint)/8192;
 end;
 
 
-function rand(x: real): boolean;
-begin
-  if x=0 then rand:=false else rand := random(maxint)/maxint < x;
-end;
-
-
-function pow(a : real; b : integer): real;
-var p : real;
-    i: integer;
-begin
-    p := 1;
-    if b = 0 then pow := 1
-    else if b > 0 then
-        for i := 1 to b do begin
-            p := p*a;
-            pow := p;
-        end
-    else pow := 1/pow(a, -b);
-end;
-
-
-function bin2dec(a: individ): real;
+{Турнирная Селекция}
+procedure selection(x: mi; var nx: mi);
 var
-    i: integer;
-    x: real;
+    i, s, l: integer;
 begin
-    x := 2*a[1] + a[2];
-    for i := 3 to n do
-        x := x + a[i]*pow(2, 2-i);
+    for i := 1 to n do
+    begin
+	    s:=random(n)+1;
+	    l:=random(n)+1;
+            if f(x[s]) < f(x[l]) then
+		nx[i]:=x[l]
+	    else
+		nx[i]:=x[s]
+    end;
+end;
 
-    bin2dec := x;
+{Перевод из 10 в 2}
+procedure dec2bin(nx: mi; var x2:mb);
+var i, j:integer;
+    c:real;
+begin
+    for i:=1 to n do
+	begin
+		case trunc(nx[i]) of
+			0:begin x2[i,1]:=0; x2[i,2]:=0 end;
+			1:begin x2[i,1]:=0; x2[i,2]:=1 end;
+			2:begin x2[i,1]:=1; x2[i,2]:=0 end;
+			3:begin x2[i,1]:=1; x2[i,2]:=1 end;
+		end;
+		c := nx[i] - trunc(nx[i]);
+		for j:=3 to s do
+		begin
+			c:=c*2;
+			x2[i,j] := trunc(c);
+			c := c - trunc(c);
+		end;
+	end;
 end;
 
 
 {Одноточечное скрещивание}
-procedure crossbreeding(var A, B, C, D : individ);
-var
-    k : integer;
-    i: integer;
-
+procedure cross(x2:mb;var xn2:mb);
+var i,j,k:integer;
 begin
-    randomize;
-    k := random(n);
-    for i:=1 to n do
-        if i<=k then begin
-            C[i] := A[i];
-            D[i] := B[i];
-        end
-        else begin
-            C[i] := B[i];
-            D[i] := C[i];
-        end;
+	for i:=1 to 5 do
+	begin
+		k:=random(16)+1;
+		for j:=1 to k do
+		begin
+			xn2[2*i-1,j]:=x2[2*i-1,j];
+			xn2[2*i,j]:=x2[2*i,j];
+		end;
+		for j:=k+1 to s do
+		begin
+			xn2[2*i-1,j]:=x2[2*i,j];
+			xn2[2*i,j]:=x2[2*i-1,j];
+		end;
+	end;
 end;
-
-
-
-{Скрещивание}
-procedure cross(var Pop: population);
-    var i, l, s: integer;
-    newpop : population;
-begin
-    for i:=1 to (n div 2) do
-        crossbreeding(pop[random(3)+1], pop[2*i-1], newpop[2*i], newpop[2*i - 1]);
-    for i:=1 to n do
-        if F(bin2dec(pop[i])) < F(bin2dec(newpop[i])) then
-            for l := 1 to n do
-                pop[i][l] := newpop[i][l];
-end;
-
 
 
 {Мутация реверс}
-procedure mutation(var pop: population);
+procedure mutation(var nx2:mb);
 var i, j, k, c : integer;
 begin
     for i:=1 to n do begin
-        randomize;
-        k := random(n-1);
-        if rand(i/n) then
-          for j:= k to ((n+k) div 2) do begin
-              c := pop[i][j];
-              pop[i][j] := pop[i][n - j + k];
-              pop[i][ n - j + k] := c;
+        k := random(s)+1;
+          for j:=1 to ((s-k) div 2) do begin
+              c := nx2[i,j+k-1];
+              nx2[i,j+k-1] := nx2[i,s-j+1];
+              nx2[i,s-j+1]:= c;
         end;
     end;
 end;
 
 
-
-procedure selection(var pop: population);
+{Перевод из 2 в 10}
+procedure bin2dec(nx2:mb; var x:mi);
 var
-    i, j, s, l: integer;
-
+    i,j:integer;
+    k:real;
 begin
-    for i := 1 to n div 2 do
-        for j := i + 1 to n do
-            if F(bin2dec(pop[i])) < F(bin2dec(pop[j])) then begin
-                for l := 1 to n do begin
-                    s := pop[i][l];
-                    pop[i][l] := pop[j][l];
-                    pop[j][l] := s;
-                end;
-            end;
+	for i:=1 to 10 do
+	begin
+		x[i]:=nx2[i,1]*2+nx2[i,2];
+		k:=0.5;
+		for j:=3 to s do
+		begin
+			x[i]:=x[i]+nx2[i,j]*k;
+			k:=k*0.5;
+		end;
+	end;
 end;
 
 
-
-
-
-procedure INIT(var pop: population);
-    var i : integer;
+{Значение функции при данных иксах}
+procedure value(a:mi;var b:mi);
+var i:integer;
 begin
-    randomize;
-    for i := 1 to n do begin
-        dec2bin(4*random(maxint)/maxint, pop[i]);
-    end;
-
+	for i:=1 to n do
+		b[i]:=f(a[i]);
 end;
 
 
-
-
-procedure loop (var pop : population; var mx : real);
-var i, j, w: integer;
+{максимум из y}
+function max(a:mi;var k:integer):real;
+var m:real;
+    i:integer;
 begin
-    for i:=1 to 100 do begin {это по количеству итераций болшая погрешность}
-        selection(pop);
-        cross(pop);
-        mutation(pop);
-        w:=1;
-        for j:=1 to n do
-          if F(bin2dec(pop[j]))>F(bin2dec(pop[w])) then
-              w:=j;
-
-        if F(bin2dec(pop[w]))>F(mx) then
-            mx := bin2dec(pop[w]);
-
-    end;
-    if F(mx) < 1.6 then begin
-      INIT(pop);
-      loop(pop, mx);
-    end;
+	m:=a[1];
+	k:=1;
+	for i:=2 to n do
+		if m<a[i] then
+		begin
+			m:=a[i];
+			k:=i;
+		end;
+	max:=m;
 end;
-
-
-
-
-
-
-
 
 
 begin
 
     {
       Ответ
-      x:= 0.779;
-      y:= 1.605;
+      x:= 0.779822;
+      y:= 1.605418;
     }
-
+    writeln('Введите количество итераций (>=0):');
+    readln(i);
     randomize;
-
-    INIT(pop);
-
-    mx := bin2dec(pop[1]);
-
-    loop(pop, mx);
-    writeln('x=', mx:3:6,' y=',F(mx):3:6);
+    INIT(x);
+    value(x,y);
+    e:=max(y,k);
+    z:=x[k];
+    while i<>0 do
+    begin
+	selection(x,nx);
+	dec2bin(nx,x2);
+	cross(x2,nx2);
+	mutation(nx2);
+	bin2dec(nx2,x);
+	value(x,y);
+	if max(y,j)>e then
+	begin
+	     e:=max(y,j);
+	     z:=x[j];
+	end;
+	dec(i);
+    end;
+    writeln('x=', z:3:6,' y=',e:3:6);
 
 
 
